7339962f301b34157fb183bfb147472046d34dc7
maxicassarino <maximocassarino@gmail.com>
2024-02-08 14:05:09 -0300

    Avances - Maxi

--- src/components/Email/EmailText.js
+++ src/components/Email/EmailText.js
@@ -1,11 +1,11 @@
 export const EmailSpanish =
 {
     h2: 'SISTEMA DE CONTACTO',
-    p: 'En TechBay, hemos desarrollado un sistema de contacto con clientes eficiente y automatizado, diseñado para simplificar el proceso de recepción y gestión de consultas a través del correo electrónico. Cuando los clientes lo contactan, nuestro sistema responde automáticamente con un mensaje de confirmación, garantizando así que la consulta fue recibida y registrada de manera oportuna.',
+    p: 'En TechBay, hemos desarrollado un sistema de contacto con clientes eficiente y automatizado, diseñado para simplificar el proceso de recepción y gestión de consultas a través del correo electrónico. Nuestro sistema responde automáticamente con un mensaje de confirmación, garantizando así que la consulta fue recibida y registrada de manera oportuna.',
 }
 
 export const EmailEnglish =
 {
     h2: 'CONTACT SYSTEM',
-    p: 'At TechBay, we have developed an efficient and automated customer contact system designed to simplify the process of receiving and managing inquiries via email. When clients reach out to us, our system automatically responds with a confirmation message, ensuring that the inquiry is received and logged promptly.',
+    p: 'At TechBay, we have developed an efficient and automated customer contact system designed to simplify the process of receiving and managing inquiries via email. Our system automatically responds with a confirmation message, ensuring that the inquiry is received and logged promptly.',
 }
