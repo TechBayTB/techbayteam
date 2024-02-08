6cb218564037abdcf473d2a90f732dcc1614d5be
maxicassarino <maximocassarino@gmail.com>
2024-02-08 15:19:00 -0300

    Avances - Maxi

--- src/components/Comments/CommentsText.js
+++ src/components/Comments/CommentsText.js
@@ -1,29 +1,25 @@
 export const titlesDataEsp = [
     {
-      comment: '“Quiero agradecerte por tus clases, cómo las preparás, cómo me exigís y la claridad para enseñar. Por otro lado, tu calidad humana”. ',
-      name: "Adrian Saa",
+      logo: "mision.png",
+      comment: 'Facilitar soluciones digitales innovadoras que potencien el crecimiento y la excelencia de nuestros clientes.',
+      name: "Misión",
     },
     {
-      comment: '"Paula es una de las personas más responsables que conozco y asume sus tareas con excelencia, respeto y compromiso”. ',
-      name: "Sofía Vilar Ragusa",
-    },
-    {
-      comment: '“Paula ha traducido varios de mis trabajos pertenecientes al área de la psicología. En su tarea de traductora ha demostrado total profesionalismo y dedicación.” ',
-      name: "Diana Goldman de Zocchi",
-    },
+      logo: "vision.png",
+      comment: 'Construir un futuro digital donde la creatividad y la tecnología se fusionen para impulsar el progreso.',
+      name: "Visión",
+    }
   ];
 
 export const titlesDataEnglish = [
     {
-        comment: '“I want to express my gratitude for your classes, how you prepare them, the way you challenge me, and the clarity in your teaching. On top of that, your kindness is truly extraordinary”. ',
-        name: "Adrian Saa",
+        logo: "mision.png",
+        comment: 'Facilitating innovative digital solutions that enhance the growth and excellence of our clients.',
+        name: "Mission",
     },
     {
-        comment: '"Paula is one of the most responsible individuals I know. She approaches her tasks with excellence, respect, and commitment”. ',
-        name: "Sofía Vilar Ragusa",
-    },
-    {
-        comment: '“Paula Páramos has translated several of my works in the field of psychology. She has shown complete professionalism and dedication.” ',
-        name: "Diana Goldman de Zocchi",
-    },  
+        logo: "vision.png",
+        comment: 'Building a digital future where creativity and technology merge to drive progress.',
+        name: "Vision",
+    } 
   ];
--- src/components/Comments/components/Message/Message.js
+++ src/components/Comments/components/Message/Message.js
@@ -3,14 +3,17 @@ import LanguageContext from "../../../../Context/Language";
 import { LanguageTypes } from "../../../Types/LanguageTypes";
 import './Message.css'
 // import { EducationEnglish, EducationSpanish } from "./TitleText";
-const Message = ({comment, name}) => {
+const Message = ({logo, comment, name}) => {
     const { language } = useContext(LanguageContext)
 
     return (
         <>
             <div className="message-container">
+                <div>
+                    <img id='messageIMG' src={require(`../../../../assets/${logo}`)}/>
+                    <p className="name">{name}</p>
+                </div>
                 <p className="comment">{comment}</p>
-                <p className="name">{name}</p>
             </div>
         </>
     );
